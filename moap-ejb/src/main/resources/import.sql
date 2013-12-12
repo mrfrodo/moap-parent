6d192c70dfbdddee7cb8a1735867a9821d8065a9
mrfrodo <golfimbulz@gmail.com>
2013-12-12 14:35:35 +0100

    correct postgresql config

--- moap-ejb/src/main/resources/import.sql
+++ moap-ejb/src/main/resources/import.sql
@@ -1,2 +1,3 @@
 -- You can use this file to load seed data into the database using SQL statements
 insert into Registrant(id, name) values (0, 'John Smith')
+insert into Registrant(id, name) values (1, 'Frodo Baggins')
